671034b279d9f668a52a0315b0a4586aeb83c84d
hicherimehdi <hicheri.mehdi@gmail.com>
2020-02-18 11:12:35 +0100

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { doesNotReject } from 'assert';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -37,7 +38,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Un petit jeu d\'allumettes');
   });
-  
+
   // On test la désactivation des bouttons suivant le joueur en partie
   it('Le joueur 1 débute la partie => le joueurs 2 doit avoir les bouttons désactivés', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -46,5 +47,14 @@ describe('AppComponent', () => {
     // J1 doit avoir les boutton activés et J2 les boutons désactivés
     expect(elementHtml.querySelector('#j1').hasAttribute('disabled')).toBeFalsy;
     expect(elementHtml.querySelector('#j2').hasAttribute('disabled')).toBeTruthy;
+  });
+
+  // On test la désactivation du bouton rejouer au début de la partie
+  it('Au début de la partie, le boutton rejouer doit être désactivé', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    // Variable qui pointe vers le HTML 
+    const elementHtml = fixture.debugElement.nativeElement;
+    // J1 doit avoir les boutton activés et J2 les boutons désactivés
+    expect(elementHtml.querySelector('#btnRejouer').hasAttribute('disabled')).toBeTruthy;
   });
 });
